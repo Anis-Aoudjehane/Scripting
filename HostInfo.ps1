20b08c479acee4a1ad3cbad6012ba777fa50f706
Anis-Aoudjehane <31874327+Anis-Aoudjehane@users.noreply.github.com>
2017-11-07 22:32:40 -0500

    Update HostInfo.ps1

--- HostInfo.ps1
+++ HostInfo.ps1
@@ -5,6 +5,16 @@ Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -fil
 exit $LASTEXITCODE
 }
 
+  Write-Host ""
+             Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+             Write-Host ""
+             Write-Host "This Script displays domain name and ip addresses informations"
+             Write-Host "                          @Anis.A                             "
+             Write-Host ""
+             Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+             Write-Host ""
+             Write-Host ""
+
   $ipTitle = "HOST Info"
   $ipMsg = "Do you want to Get Host Info?"
 
@@ -14,38 +24,142 @@ exit $LASTEXITCODE
       $ipNo = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
            "Exit"
 
-$ipOptions = [System.Management.Automation.Host.ChoiceDescription[]]($ipYes, $ipNo)
+      $ipOptions = [System.Management.Automation.Host.ChoiceDescription[]]($ipYes, $ipNo)
 
-$ipResult = $host.ui.PromptForChoice($ipTitle, $ipMsg, $ipOptions, 0) 
+      $ipResult = $host.ui.PromptForChoice($ipTitle, $ipMsg, $ipOptions, 0) 
 
 switch ($ipResult)
     {
         0 {
-            $hostName= read-host "enter host name"
              Write-Host ""
-             Write-Host "_______________________"
+             Write-Host "..........................."
+            $hostName= read-host "Enter Host Name"
+             Write-Host "..........................."
+             Write-Host ""
+             Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+             Write-Host ""
                Write-Host "Name Servers"
-               nslookup $hostName
+                 nslookup $hostName
+                 nslookup -queytype=soa $hostName
+
             
-            
-             $ping= ping $hostName
-            
-            
-            Write-Host "_______________________"
-            Write-Host "Ping Test"
+                       Write-Host ""
+            Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
             Write-Host ""
-            $ping
-            Write-Host "_______________________"
+               Write-Host "Ping Test"
+            Write-Host ""
+            
+            $ping= ping $hostName
+            
+ 
+               $ping
+            Write-Host ""
+            Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+            
          
-            Write-Host ""
-            tracert $hostName
-           
-           
-           }
+          }
         
         1 {exit}
     }
+
+
+$traceTitle = "TRACEROUTE"
+
+$traceMsg = "Do you want to run Traceroute?"
+
+      $traceYes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+           "Start a traceroute to the server"
+
+      $traceNo = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+           "Exit"
+
+$traceOptions = [System.Management.Automation.Host.ChoiceDescription[]]($traceYes, $traceNo)
+
+$traceResult = $host.ui.PromptForChoice($traceTitle, $traceMsg, $traceOptions, 0) 
+
+switch ($traceResult)
+    {
+        0 {tracert $hostName}
+           
+        1 {break}
+    }
+
+    Write-Host ""
+             Write-Host "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+             Write-Host ""
+    $portTitle = "Port Scanning"
+    Write-Host ""
+
+$portMsg = "Do you want to check if ports are open?"
+
+      $portYes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+           "Start a traceroute to the server"
+
+      $portNo = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+           "Exit"
+
+$portOptions = [System.Management.Automation.Host.ChoiceDescription[]]($portYes, $portNo)
+
+$portResult = $host.ui.PromptForChoice($portTitle, $portMsg, $portOptions, 0) 
+
+switch ($portResult)
+    {
+        0 {          
+            
+            
+                        $pTitle = ""
+                        Write-Host ""
+                        Write-Host "What type of scan do you want to perform?"
+                        Write-Host "             1. Common Ports (80,443,21,22,25,53,139,143,110,113)"
+                        Write-Host "             2. Specific Port"
+                        $pMsg = ""
+                       
+
+                        $p1 = New-Object System.Management.Automation.Host.ChoiceDescription "&1", `
+                              "Scan only common ports"
+
+                        $p2 = New-Object System.Management.Automation.Host.ChoiceDescription "&2", `
+                              "Enter the port you want to Scan"
+
+                        $pOptions = [System.Management.Automation.Host.ChoiceDescription[]]($p1, $p2)
+
+                        $pResult = $host.ui.PromptForChoice($pTitle, $pMsg, $pOptions, 0) 
+                         
+
+                               switch ($pResult)
+                                      {
+                                            0 { 
+                                                    Test-NetConnection -Port 80 -ComputerName $hostName
+                                                    Test-NetConnection -Port 443 -ComputerName $hostName
+                                                    Test-NetConnection -Port 21 -ComputerName $hostName
+                                                    Test-NetConnection -Port 22 -ComputerName $hostName
+                                                    Test-NetConnection -Port 23 -ComputerName $hostName
+                                                    Test-NetConnection -Port 25 -ComputerName $hostName
+                                                    Test-NetConnection -Port 53 -ComputerName $hostName
+                                                    Test-NetConnection -Port 139 -ComputerName $hostName
+                                                    Test-NetConnection -Port 143 -ComputerName $hostName
+                                                    Test-NetConnection -Port 110 -ComputerName $hostName
+                                                    Test-NetConnection -Port 113 -ComputerName $hostName
+                                               } 
+           
+       
+                                             1 {     
+                                                    Write-Host ""
+                                                    $portEdit= read-host "Enter the port number"
+
+                                                    Test-NetConnection -Port $portEdit -ComputerName $hostName
+        
+                                               }
+                                       }
  
+            
+            
+            }
+
+           
+        1 {break}
+    }
+
  
  
  pause
